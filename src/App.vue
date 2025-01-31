6377a7a6f19a35bb10875beea78606b6eceff42e
lcj24 <chuljooleel@gmail.com>
2025-01-31 22:21:33 +0900

    셋팅

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+
+<script setup>
+import Skip from "./Skip.vue";
+import Header from "./Header.vue";
+import Intro from './Intro.vue';
+import Skill from "./Skill.vue";
+import Site from "./Site.vue";
+import Port from "./Port.vue";
+import Contact from "@/components/Contact.vue";
+import Footer from "./Footer.vue";
+</script>
+
+<template>
+  <main>
+    <Skip />
+    <Header></Header>
+    <main id="main" role="main">
+      <Intro></Intro>
+      <Skill></Skill>
+      <Site></Site>
+      <Port></Port>
+      <Contact></Contact>
+    </main>
+    <Footer></Footer>
+  </main>
+</template>
